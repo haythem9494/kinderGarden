bd0979dac603ef846df90bc2327bfd2ef0a11b39
haythem bekir <haythem.bekir@gmail.com>
2018-01-09 13:29:45 +0100

    parfait

--- SB-Admin-BS4-Angular-5-master/src/app/layout/create-parent/create-parent.module.ts
+++ SB-Admin-BS4-Angular-5-master/src/app/layout/create-parent/create-parent.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 import { FormRoutingModule } from './create-parent-routing.module';
 import { CreateParentComponent } from './create-parent.component';
 import { PageHeaderModule } from './../../shared';
+import { HttpClientModule } from '@angular/common/http';
+ import { HttpModule } from '@angular/http';
+
 
 @NgModule({
-    imports: [CommonModule, FormRoutingModule, PageHeaderModule],
+    imports: [
+        
+        CommonModule, 
+        FormRoutingModule, 
+        PageHeaderModule,
+        FormsModule,
+        ReactiveFormsModule,HttpClientModule,HttpModule
+
+    ],
     declarations: [CreateParentComponent]
 })
 export class CreateParentModule {}
